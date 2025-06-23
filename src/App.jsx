fc3d081a200381378117d96a9c4e402eda275bf1
pr9av <baliyanpranav5@gmail.com>
2025-06-24 00:16:07 +0530

    ag2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Services from './components/Services/Services'
-import MyWork from  './components/Mywork/MyWork'
+import MyWork from  './components/MyWork/MyWork'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
